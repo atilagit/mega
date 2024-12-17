433a478176c6725762edb53feecd58b08ae0c6d5
Atila Silva <atila__7@hotmail.com>
2024-12-17 08:57:08 -0300

    AlreadyExists code smell

--- src/main/java/org/example/utils/AlreadyExists.java
+++ src/main/java/org/example/utils/AlreadyExists.java
@@ -6,14 +6,15 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AlreadyExists {
     public static final List<List<Integer>> alreadExistList = getAlreadExistList();
 
     public static List<List<Integer>> getAlreadExistList() {
-        try {
-            String filePath = "C:\\temp\\in.csv"; // Caminho do arquivo
-            List<List<Integer>> result = Files.lines(Paths.get(filePath))
+        String filePath = "C:\\temp\\in.csv"; // Caminho do arquivo
+        try (Stream<String> lines = Files.lines(Paths.get(filePath))) { // Try-with-resources
+            return lines
                     .map(line -> Arrays.stream(line.split(","))
                             .skip(Math.max(0, line.split(",").length - 6)) // Pega apenas os últimos 6 valores
                             .map(String::trim) // Remove espaços extras
@@ -21,8 +22,6 @@ public class AlreadyExists {
                             .sorted() // Ordena os valores
                             .collect(Collectors.toList()))
                     .collect(Collectors.toList());
-
-            return result;
         } catch (IOException e) {
             throw new RuntimeException("Erro ao ler o arquivo: " + e.getMessage());
         }
