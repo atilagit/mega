f3633681872810b721863ca818ab83227645d717
Atila Silva <atila__7@hotmail.com>
2024-12-17 07:47:51 -0300

    Code smell

--- src/main/java/org/example/validations/TodosNaMesmaDezena.java
+++ src/main/java/org/example/validations/TodosNaMesmaDezena.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 public class TodosNaMesmaDezena {
     public static boolean validar(List<Integer> list) {
-        int posicaoUltimoElemento = list.size()-1;
-        if ((list.get(posicaoUltimoElemento) - list.get(0)) < 10){
-            return true;
-        }
-        return false;
+        return (list.getLast() - list.getFirst()) < 10;
     }
 }
